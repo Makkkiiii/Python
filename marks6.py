fc2946e97f988f2303172202fe9247d6fc0b69ce
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-19 09:55:02 +0545

    Update marks6.py

--- marks6.py
+++ marks6.py
@@ -1,11 +1,11 @@
 marks = int(input("Enter your marks: "))
-if marks <=80 and marks >=100:
+if marks <=100 and marks >=80:
     print("A")
-elif marks <=60 and marks >=80:
+elif marks <=80 and marks >=60:
     print("B")
-elif marks <=40 and marks >=60:
+elif marks <=60 and marks >=40:
     print("C")
-elif marks <=20 and marks >=40:
+elif marks <=40 and marks >=20:
     print("D")
 else:
     print("Fail")
