bb5f8ec8e68a9a392c6479ea70bfd876f6a2f5d1
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-20 13:33:01 +0545

    Update pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -1,39 +1,47 @@
+#1
 for i in range (1,6):
     print((str(i)*i))
 print()
 
+#2
 for i in range (1,6):
    print('*' *i)
 print()
 
+#3
 for i in range (5,0,-1):
     print('*' *i)
 print()
 
-
+#4
 for i in range (1,5):
      print(' ' * (4 - i) + '*' * i)
 print()
 
+#5
 for i in range (5, 0,-1):
     print(' ' * (5 - i) + '*' * i)
 print()
 
+#6
 ch = 'SUMAN'
 for i in range(1,6):
     print(ch[:i])
 print()
 
+#7
 ch = 'SUMAN'
 for i in range(5,0,-1):
     print(ch[:i])
 print()
 
+#8
 ch = 'SUMAN'
 for i in range(1,6):
     print(ch[:i].rjust(5))
 print()
 
+#9
 ch = 'SUMAN'
 for i in range(5, 0, -1):
     print(ch[:i].rjust(5))
