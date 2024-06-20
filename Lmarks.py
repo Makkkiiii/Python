09bd3e3f8d43a4edf2bacf43ff83bfa746886d01
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-20 13:40:36 +0545

    Update Lmarks.py

--- Lmarks.py
+++ Lmarks.py
@@ -1,21 +1,16 @@
 marks = [60,67,45,87,90,78,84,38,13,56]
 
 # A
-a = []
 total = 0
+count = 0
 for mark in marks:
     if mark >= 60:
-        a.append(mark)
+        count += 1
         total += mark
-b = len(a)
 
 # B
-is_score_13 = False
-for mark in marks:
-    if mark == 13:
-        is_score_13 = True
-        break
+is_score_13 = 13 in marks
 
-print("A. 60 or more: ", b)
+print("A. 60 or more: ", count)
 print("A. Sum: ", total)
 print("B. Who scored 13: ", is_score_13)
