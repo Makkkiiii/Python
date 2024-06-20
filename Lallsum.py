28d85978ddf8c05fc863b32b5bbfaadb62bf0ac7
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-20 13:45:46 +0545

    Update Lallsum.py

--- Lallsum.py
+++ Lallsum.py
@@ -1,8 +1,17 @@
 # WAP to find the sum of all the digits of a number enter by the user
 
+#WHILE LOOP
 num = int(input("Enter the number: "))
 sum = 0
 while num > 0:
     sum = sum + num % 10
     num = num // 10
+print("Sum of all = ", sum)
+
+
+#FOR LOOP
+num = int(input("Enter the number: "))
+sum = 0
+for i in str(num):
+    sum += int(i)
 print("Sum of all = ", sum)
