b95dc0321c9f43a1596f3174d25f30dcc58d9498
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-21 09:24:30 +0545

    Update LFibo.py

--- LFibo.py
+++ LFibo.py
@@ -1,7 +1,21 @@
 #WAP for Fibonacci series
+
+#WHILE LOOP
 num = int(input("Enter the number: "))
 n1 = 0
 n2 = 1
 while n1 < num:
     print(n1, end = " ")
     n1, n2 = n2 , n1+n2
+
+
+
+#FOR LOOP
+
+num = int(input("Enter the number: "))
+n1, n2 = 0, 1
+for i in range(num):
+    if n1 > num:
+        break
+    print(n1, end=" ")
+    n1, n2 = n2, n1 + n2
