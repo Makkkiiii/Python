fe057e5ddeb94b6ca7acd3156ab4fb3adbe36d02
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-21 09:58:50 +0545

    Update Larm.py

--- Larm.py
+++ Larm.py
@@ -22,3 +22,17 @@ if sum == int(num):
     print("Armstrong")
 else:
     print("Not Armstrong")
+
+#For n numbers
+
+arm = int(input("Enter the number: ")
+sum = 0
+l = len(arm)
+
+for i in arm:
+    n = int(i)
+    sum = sum + n ** l
+if sum == int(arm):
+    print("Armstrong")
+else:
+    print("Not Armstrong")
