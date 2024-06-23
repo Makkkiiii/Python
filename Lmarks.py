2f48b491a9808cf6c10449ec50b34885d89e2cae
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-23 12:42:10 +0545

    Update Lmarks.py'

--- Lmarks.py
+++ Lmarks.py
@@ -14,3 +14,25 @@ is_score_13 = 13 in marks
 print("A. 60 or more: ", count)
 print("A. Sum: ", total)
 print("B. Who scored 13: ", is_score_13)
+
+#WHILE LOOP
+
+marks = [60,67,45,87,90,78,84,38,13,56]
+
+total = 0
+count = 0
+index = 0
+is_score_13 = False
+
+while index < len(marks):
+    mark = marks[index]
+    if mark >= 60:
+        count += 1
+        total += mark
+    if mark == 13:
+        is_score_13 = True
+    index += 1
+
+print("A. 60 or more: ", count)
+print("A. Sum: ", total)
+print("B. Who scored 13: ", is_score_13)
