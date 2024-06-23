e8d0203ba988d50f86cbbca1e9f8dd9436ba173f
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-23 12:31:55 +0545

    Update Lsum.py

--- Lsum.py
+++ Lsum.py
@@ -5,3 +5,13 @@ sum = 0
 for i in range(1,num+1):
     sum = sum + i
     print("The sum of n natural number is: ",sum)
+
+
+# While Loop
+a = int(input("Enter the number: "))
+i = 1
+sum = 0
+while i<=a:
+    sum += i
+    i = i + 1
+print('The sum is = ', sum)
