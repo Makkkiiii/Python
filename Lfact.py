cd5e6d94d9246a8df161251151d0f04922a4578a
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-23 12:33:02 +0545

    Update Lfact.py

--- Lfact.py
+++ Lfact.py
@@ -5,3 +5,13 @@ fact = 1
 for i in range(1,num+1):
     fact = fact * i
 print("The factorial of a number is: ",fact)
+
+# While Loop
+
+a = int(input("Enter the number: "))
+i = 1
+fact = 1
+while i<=a:
+    fact *= i
+    i = i + 1
+print('The fact is = ', fact)
