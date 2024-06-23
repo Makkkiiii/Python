53f47299eba918786b45e53b758a8edd24fc9493
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-23 12:39:54 +0545

    Update Lvowel.py

--- Lvowel.py
+++ Lvowel.py
@@ -4,3 +4,17 @@ for i in ch:
     if i in "aeiou":
         count+=1
 print("No. of vowel = ", count)
+
+
+#While Loop
+
+ch = input("Enter the string: ")
+
+count = 0
+index = 0 
+while index < len(ch):
+    if ch[index].lower() in 'aeiou':  
+        count += 1
+    index += 1  
+
+print("The number of vowels are: ", count)
