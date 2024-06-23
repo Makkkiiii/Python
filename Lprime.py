eae93d9d3b038ebc57885ed200a8cbc6c61826be
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-23 12:50:25 +0545

    Update Lprime.py

--- Lprime.py
+++ Lprime.py
@@ -8,3 +8,18 @@ if count == 2:
     print("Prime")
 else:
     print("Not Prime")
+
+
+#While Loop
+
+num  = int(input("Enter the number: "))
+count = 0 
+i = 1
+while i <= num:
+    if num % 1 == 0:
+        count += 1
+        i += 1
+if count == 2:
+        print ("Prime")
+else:
+        print("Not Prime")
