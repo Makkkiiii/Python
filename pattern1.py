b70da6b7689acaf15493ed26b4f4f2fdb1c2a988
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-23 13:20:39 +0545

    Update pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -46,6 +46,67 @@ ch = 'SUMAN'
 for i in range(5, 0, -1):
     print(ch[:i].rjust(5))
 
+#WHILE LOOPS
+
+#1. 
+i = 1
+while i < 6:
+    print(str(i) * i)
+    i += 1
+
+#2
+i = 1
+while i < 6:
+    print('*' *i)
+    i += 1
+
+#3
+i = 5
+while i > 0:
+    print('*' *i)
+    i -= 1
+
+#4
+i = 1
+while i < 5:
+    print(' ' * (4 - i) + '*' * i)
+    i += 1
+
+#5
+i = 5
+while i > 0:
+    print(' ' * (5 - i) + '*' * i)
+    i -= 1
+
+#6
+ch = 'SUMAN'
+i = 1
+while i <= len(ch):
+    print(ch[:i])
+    i = i + 1   
+
+#7 
+ch = 'SUMAN'
+i = 5
+while i > 0:
+    print(ch[:i])
+    i -= 1
+
+#8 
+ch = 'SUMAN'
+i = 1
+while i <= len(ch):
+    print(ch[:i].rjust(5))
+    i += 1
+
+#9
+ch = 'SUMAN'
+i = len(ch)
+while i > 0:
+    print(ch[:i].rjust(5))
+    i -= 1
+
+
 '''
 1
 22
