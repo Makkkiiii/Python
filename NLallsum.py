cd5f54bed1d13d3b0c3ce954ed753b17dba999e9
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-26 10:16:56 +0545

    Update NLallsum.py

--- NLallsum.py
+++ NLallsum.py
@@ -2,8 +2,8 @@ n = int(input("Enter a number: "))
 
 while n > 9:
     sum = 0
-    for digit in str(n):
-        sum += int(digit)
+    for i in str(n):
+        sum += int(i)
     n = sum
 
 print("Single digit number: ", n)
