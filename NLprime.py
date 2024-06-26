00c496b67c8a36261fd155abeb9b7ec2f166bcd1
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-06-26 09:29:30 +0545

    Update NLprime.py

--- NLprime.py
+++ NLprime.py
@@ -1,5 +1,5 @@
 a = [2,4,5,6,7,8,13,15,17,20]
-prime_numbers = []
+prime_count = 0
 
 for num in a:
     if num > 1:
@@ -7,9 +7,9 @@ for num in a:
         for i in range(2, num):
             if (num % i) == 0:
                 is_prime = False
-                i = num
+                break
         if is_prime:
-            prime_numbers.append(num)
+            print("Prime number:", num)
+            prime_count += 1
 
-print("Prime numbers: ", prime_numbers)
-print("Total number of prime numbers: ", len(prime_numbers))
+print("Total number of prime numbers:", prime_count)
