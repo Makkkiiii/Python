eadd5c7a4217bacc5dcb047776f4fbca9d81aba9
Denish Maharjan <148240694+Makkkiiii@users.noreply.github.com>
2024-07-04 12:51:01 +0545

    Update Fcommonel.py

--- Fcommonel.py
+++ Fcommonel.py
@@ -4,3 +4,15 @@ def common_elements(lst1, lst2):
 lst1 = [1, 2, 3, 4, 5]
 lst2 = [4, 5, 6, 7, 8]
 print(common_elements(lst1, lst2))
+
+#Using Loop
+def common_elements(lst1, lst2):
+    common = []
+    for i in lst1:
+        if i in lst2 and i not in common:
+            common.append(i)
+    return common
+
+lst1 = [1, 2, 3, 4, 5]
+lst2 = [4, 5, 6, 7, 8]
+print(common_elements(lst1, lst2))
